c505ab26bfcd6427fe38e599238fe91c2cadaba9
scott.kim <scottjkim777@gmail.com>
2020-02-05 02:43:28 +0800

    partial, edit, show, delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,38 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
   def new
     @article = Article.new
   end
+
+  #edit
+  def edit
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound => e
+    flash[:notice] = "Record Not Found"
+    redirect_to new_article_path()
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    if @article.destroy
+      flash[:notice] = "Article deleted"
+      redirect_to articles_path
+    end
+  end
+
+  #create
   def create
     #render plain: params[:article].inspect
     # whitelist the
@@ -17,6 +48,8 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
+  #show
   def show
     @article = Article.find(params[:id])
   end
