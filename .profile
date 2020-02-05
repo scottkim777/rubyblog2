4f9078602841be87da8305257b8bb5c725d0547e
scott.kim <scottjkim777@gmail.com>
2020-02-06 07:18:38 +0800

    create users table and model

---
+++ .profile
@@ -0,0 +1,1 @@
+export PS1="\\w:\(\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\$) "
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_180910) do
+ActiveRecord::Schema.define(version: 2020_02_05_231033) do
 
   create_table "articlers", force: :cascade do |t|
   end
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_02_03_180910) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
