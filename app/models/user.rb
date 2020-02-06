6a579907e455e2d652d2c5ae615a34b1bf3900af
scott.kim <scottjkim777@gmail.com>
2020-02-06 08:58:04 +0800

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: { case_sensitive: false},
+             length: {minimum: 3, maximum: 25}
+             
+  # we validate our email using regex
+  VALID_EMAIL_REGEX= /\w*\@\w*\.\w*/
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
 end
